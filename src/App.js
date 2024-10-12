612b207581c92b24ed6878df543c46822147fab6
Gary <garygdspd@gmail.com>
2024-10-13 00:06:21 +0300

    Completed project

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import './App.css';
 
 function App() {
@@ -16,13 +15,18 @@ function App() {
       iterations: 1
     }).onFinish = () => {
       mainApp.style.backgroundColor = newColor;
-      event.target.style.color = newColor;
+      event.target.style.backgroundColor = newColor;
     };
 
   }
 
   return (
     <main id="mainApp">
+      <div id='profileContainer'>
+        <button className='profileButton'><a href='https://www.instagram.com/urban.chaos007/' target='_blank'><i class="fa-brands fa-instagram profile"></i></a></button>
+        <button className='profileButton'><a href='https://www.tiktok.com/@urbanchaos2024?lang=en' target='_blank'><i class="fa-brands fa-tiktok profile"></i></a></button>
+      </div>
+      
       <div id="buttonContainer">
         <button id="button" onClick={changeColor}>Change Color</button>
       </div>
